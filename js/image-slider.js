dbdffeb179f83e15cf51f1760af25ea75c3785be
paragon0277 <79505217+paragon0277@users.noreply.github.com>
2021-04-29 12:33:23 +0200

    Update image-slider.js

--- js/image-slider.js
+++ js/image-slider.js
@@ -1,57 +1,26 @@
-var imageCounter = 1;
-
-showDiv(imageCounter);
-
-function changeImage(m) {
-
-showDiv(imageCounter = imageCounter + m);
-
+<script>
+var slideIndex = 1;
+function showSlides() {
+	var slides = document.getElementsByClassName("mySlides");
+	if (slideIndex > slides.length) {slideIndex = 1;}
+	if (slideIndex < 1) {slideIndex = slides.length;}
+	for (var i = 0; i < slides.length; i++) {
+		slides[i].style.display = "none";
+	}
+	slides[slideIndex-1].style.display = "block";
+	var dots = document.getElementsByClassName("slide-dot");
+	for (var i = 0; i < dots.length; i++) {
+		dots[i].className = dots[i].className.replace(" slide-dot-active", "");
+	}
+	dots[slideIndex-1].className += " slide-dot-active";
+ }
+function plusSlides(n) {
+	slideIndex += n;
+	showSlides();
 }
-
-function showDiv(n) {
-
-var i;
-
-var imageArray = document.getElementsByClassName("mySlides");
-
-if (n > imageArray.length) {
-
-imageCounter = 1;
-
+function currentSlide(n) {
+	slideIndex = n;
+	showSlides();
 }
-
-if (n < 1) {
-
-imageCounter = imageArray.length;
-
-}
-
-for (i = 0; i < imageArray.length; i++) {
-
-imageArray[i].style.display = "none";
-
-}
-
-imageArray[imageCounter - 1].style.display = "block";
-
-}
-
-
-if (n > x.length) {
-
-imageCounter = 1;
-
-}
-
-if (n < 1) {
-
-imageCounter = x.length;
-
-}
-
-
-for (i = 0; i < imageArray.length; i++) {
-
-imageArray[i].style.display = "none";
-
-}
+window.onload = showSlides;
+</script>
